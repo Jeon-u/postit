a3d40d3fbbdea90c683b9ff6fff5bad70898912d
Yujeong Jeon <yj_jeon@nextree.io>
2023-07-02 18:14:41 +0900

    feat: Add PostItApplication

---
+++ src/test.java.com.alpaca.postit/PostItApplicationTests.java
@@ -0,0 +1,14 @@
+package com.alpagram.alpagrambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class PostItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
